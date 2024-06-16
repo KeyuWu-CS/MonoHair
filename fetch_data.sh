0f8f7816c26e0b5662f799f8744398850ed0306c
wky1998 <1574717008@qq.com>
2024-06-16 19:29:08 +0800

    update fetch data

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,39 +1,11 @@
-# trained model for RobustVideoMatting
-mkdir -p assets/MODNet
-echo -e "Downloading MODNet model..."
-# https://drive.google.com/file/d/1Nf1ZxeJZJL8Qx9KadcYYyEmmlKhTADxX/viewmkdir
-FILEID=1Nf1ZxeJZJL8Qx9KadcYYyEmmlKhTADxX
-FILENAME=assets/MODNet/modnet_webcam_portrait_matting.ckpt.pth
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${FILEID} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
-#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
-
-
-
-# trained model for face-parsing
-# if failed, please download the model from
-# https://drive.google.com/file/d/154JgKpzCPW82qINcVieuPH3fZ2e0P812/view
-mkdir -p assets/face_parsing
-echo -e "Downloading face_parsing model..."
-FILEID=154JgKpzCPW82qINcVieuPH3fZ2e0P812
-FILENAME=assets/face_parsing/model.pth
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${FILEID} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
-# wget "https://drive.google.com/file/d/154JgKpzCPW82qINcVieuPH3fZ2e0P812/view" -o "assets/face_parsing/model.pth"
-
-
-# trained model for PIXIE
-# if failed, please check https://github.com/yfeng95/PIXIE/blob/master/fetch_model.sh
-cd submodules/PIXIE
-echo -e "Downloading PIXIE data..."
-#!/bin/bash
-urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
-
 # SMPL-X 2020 (neutral SMPL-X model with the FLAME 2020 expression blendshapes)
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 echo -e "\nYou need to register at https://smpl-x.is.tue.mpg.de"
 read -p "Username (SMPL-X):" username
 read -p "Password (SMPL-X):" password
 username=$(urle $username)
 password=$(urle $password)
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O './data/SMPLX_NEUTRAL_2020.npz' --no-check-certificate --continue
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O './assets/data/SMPLX_NEUTRAL_2020.npz' --no-check-certificate --continue
 
 # PIXIE pretrained model and utilities
 echo -e "\nYou need to register at https://pixie.is.tue.mpg.de/"
@@ -41,14 +13,30 @@ read -p "Username (PIXIE):" username
 read -p "Password (PIXIE):" password
 username=$(urle $username)
 password=$(urle $password)
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=pixie_model.tar&resume=1' -O './data/pixie_model.tar' --no-check-certificate --continue
-wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=utilities.zip&resume=1' -O './data/utilities.zip' --no-check-certificate --continue
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=pixie_model.tar&resume=1' -O './assets/data/pixie_model.tar' --no-check-certificate --continue
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=utilities.zip&resume=1' -O './assets/data/utilities.zip' --no-check-certificate --continue
+cd ./assets/data
+unzip utilities.zip
+rm utilities.zip
+cd ../..
 
-cd ..
+# detla utilities
+echo -e "\nDownloading delta data..."
+wget https://nextcloud.tuebingen.mpg.de/index.php/s/zR3DM3zEdje984c/download -O ./assets/data/delta_utilities.zip
+unzip ./assets/data/delta_utilities.zip -d ./assets/data
+mv ./assets/data/delta_utilities/* ./assets/data/
+rm ./assets/data/delta_utilities.zip
+rm -rf ./assets/data/delta_utilities
+rm -rf ./assets/data/__MACOSX
+
+
+
 echo -e "\nDownloading more data..."
-wget https://nextcloud.tuebingen.mpg.de/index.php/s/jraekdRrxCzYEWB/download -O ./data/delta_utilities2.zip
-unzip ./data/delta_utilities2.zip -d ./data
-mv ./data/delta_utilities2/* ./data/
-rm ./data/delta_utilities2.zip
-rm -rf ./data/delta_utilities2
-cd ../process_data
+wget https://nextcloud.tuebingen.mpg.de/index.php/s/jraekdRrxCzYEWB/download -O ./assets/data/delta_utilities2.zip
+unzip ./assets/data/delta_utilities2.zip -d ./assets/data
+mv ./assets/data/delta_utilities2/* ./assets/data/
+rm ./assets/data/delta_utilities2.zip
+rm -rf ./assets/data/delta_utilities2
+
+
+
